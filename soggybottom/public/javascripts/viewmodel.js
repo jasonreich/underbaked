40223d7e9fd8a939ca5d5a0b28311a2ab7eb8154
Jason Reich <jason@cs.york.ac.uk>
2013-12-08 15:47:42 +0000

    Broken start

--- soggybottom/public/javascripts/viewmodel.js
+++ soggybottom/public/javascripts/viewmodel.js
@@ -12,38 +12,96 @@ var nextColor = (function() {
 })();
 
 // Trace ViewModels
-var Trace = function() {
+var Trace = function(map) {
   var self = this;
 
-  // Static properties
-  self.title = 'Hello world';
+  // Plain properties
   self.color = nextColor();
-  self.startDate = new Date();
+  var poly = new google.maps.Polyline({
+      strokeColor: self.color,
+      strokeOpacity: 0.7,
+      strokeWeight: 4,
+      map: map
+  });
 
-  // Dynamic properties
-  self.visible = ko.observable(true);
+  // Knockout properties
+  self.title = ko.observable('Loading...');
+  self.startDate = ko.observable(new Date());
   self.endDate = ko.observable();
   self.points = ko.observableArray();
+  self.visible = ko.observable(true);
 
   // Calculated properties
   self.startDateView = ko.computed(function() {
-    return moment(self.startDate).calendar();
+    return moment(self.startDate()).calendar();
   });
   self.visibleView = ko.computed(function() {
     return self.visible() ? '&#9673;' : '&#9678;';
   });
   self.totalTimeView = ko.computed(function() {
     if (self.endDate()) {
-      return moment(self.endDate()).diff(self.startDate, 'hours', true) + ' hours';
+      return moment(self.endDate()).diff(self.startDate(), 'hours', true) + ' hours';
     } else {
       return '<span class="text-success live">LIVE...</span>';
     }
+  }); /*
+  self.pointsPolyView = ko.computed(function() {
+    return $.map(self.points(), function(obj) {
+      return obj.p;
+    });
   });
+  self.pointsSpeedView = ko.computed(function() {
+    var timeSeries = [];
+    var windowTime = 60;
+    var objQueue = [];
+    self.points().forEach(function(obj) {
+      objQueue.push(obj);
+      var timeDelta = moment(objQueue[0].time).diff(obj.time, 'seconds', true);
+      if(objQueue.length > 2 && windowTime <= timeDelta) {
+        var oldObj = objQueue.shift();
+        var distance = google.maps.geometry.spherical
+            .computeDistanceBetween(oldObj.p, obj.p) / 1000;
+        var minutes = moment(oldObj.time).diff(self.startTime(), 'minutes', true);
+        timeSeries.push({x: minutes, y : distance / timeDelta});
+      }
+    });
+    return timeSeries;
+  }); */
 
   // Methods
   self.toggleVisible = function() {
     self.visible(!self.visible());
   };
+
+  // Subscriptions
+  // self.pointsPolyView.subscribe(function(newValue) {
+  //   poly.setPath(newValue);
+  // });
+  // self.visible.subscribe(function(newValue) {
+  //   poly.setOptions({strokeOpacity : newValue ? 0.7 : 0.4});
+  // });
+
+  console.log("Start load.");
+  $.ajax({
+      type: 'GET',
+      url: './my_route.gpx',
+      dataType: 'text',
+      success: function(xml) {
+        console.log("Start parse.");
+        var $xml = $(xml);
+        var $trkpts = $xml.children('trk')
+                    .children('trkseg').children('trkpt');
+        self.startDate(new Date($trkpts.first().find('time').text()));
+
+        $trkpts.each(function() {
+          var lat = $(this).attr('lat');
+          var lng = $(this).attr('lon');
+          var time = new Date($(this).find('time').text());
+          var p = new google.maps.LatLng(lat, lng);
+          self.points.push({time: time, p: p});
+        });
+      }
+  });
 };
 
 // Page ViewModel
@@ -53,7 +111,7 @@ var ViewModel = function(map, chart, chartData) {
   // Traces
   self.traces = ko.observableArray();
   self.addTrace = function() {
-    self.traces.push(new Trace());
+    self.traces.push(new Trace(map));
   };
 };
 
